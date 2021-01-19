f3746046296a11a0e35fe086feb5cf843a22f03a
Zafer <zaferkok111@gmail.com>
2021-01-19 01:06:14 +0100

    2

--- src/demopackage/democlass.java
+++ src/demopackage/democlass.java
@@ -3,6 +3,8 @@ package demopackage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.swing.*;
+
 public class democlass {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\CIMBOM\\Documents\\selenium dependencies\\drivers\\chromedriver_win32\\chromedriver.exe");
@@ -10,5 +12,6 @@ public class democlass {
         driver.get("https://www.google.com/");
         System.out.println("Alles klar!");
         System.out.println("Ja!");
+        System.out.println("Wie geht es dir?");
     }
 }
